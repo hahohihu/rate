3e24ea204e2d814ce6e018d1559e23e9005b7d9d
Peter Shen <pshen4@gmail.com>
2024-04-23 16:28:18 -0700

    only show month once

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,33 @@ import './diary.css';
 
 export default async function Home() {
   let movies = await fetchMovies();
+  let prevMonth: number;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <table>
         <thead>
           <tr>
-            {["Name", "Released", "Rating"].map(header => (<td>{header}</td>))}
+            {["Month", "Date", "Name", "Released", "Rating"].map(header => (<td>{header}</td>))}
           </tr>
         </thead>
         <tbody>
-          {movies?.map(movie => (
-            <tr>
-              <td>{movie.name}</td>
-              <td>{movie.release}</td>
-              <td>{movie.rating}</td>
-            </tr>
-          ))}
+          {movies?.map(movie => {
+            let currentMonth = movie.watch.getMonth();
+            let monthYear;
+            if (currentMonth != prevMonth) {
+              monthYear = movie.watch.toLocaleString('default', { month: 'long' });
+              prevMonth = currentMonth;
+            }
+            return (
+              <tr>
+                <td>{monthYear}</td>
+                <td>{movie.watch.getDate()}</td>
+                <td>{movie.name}</td>
+                <td>{movie.release}</td>
+                <td>{movie.rating}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </main>
