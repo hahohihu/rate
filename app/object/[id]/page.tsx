6c8c666d68531692f0c887299fb654990d5be4f7
Peter Shen <pshen4@gmail.com>
2024-04-25 16:34:58 -0700

    add ui for add new entry button

--- app/object/[id]/page.tsx
+++ app/object/[id]/page.tsx
@@ -1,12 +1,18 @@
 import ObjectInfo from "./object-info";
 import Entries from "./entries";
 import styles from './layout.module.css';
+import Link from "next/link";
 
 export default async function Home({ params }: { params: { id: number } }) {
     return (
         <main className={`p-4 ${styles.object_page}`}>
             <ObjectInfo id={params.id} />
             <div className={styles.entries}>
+                <Link href={`/add/${params.id}`} className="action-link" >
+                    <div className={styles.add_entry}>
+                        add a new entry
+                    </div>
+                </Link>
                 <Entries objectId={params.id}/>
             </div>
         </main >
