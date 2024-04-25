aee53c1347e09952daae21d8fced0dfc95308389
Peter Shen <pshen4@gmail.com>
2024-04-25 10:51:46 -0700

    nav logo

--- ui/header/header.tsx
+++ ui/header/header.tsx
@@ -1,13 +1,24 @@
 'use client';
 
-import SearchBar from "../searchbar/searchbar";
+import SearchBar from "@/ui/searchbar/searchbar";
 import logo from '@/public/logo.svg';
 import Image from 'next/image'
+import Link from "next/link";
+
+// {links.map(link => (
+//     <Link key={link.name} href={link.href} className={clsx("py-2 px-4", {
+//         'active-nav': pathname == link.href
+//     })}>
+//         {link.name}
+//     </Link>
+// ))}
 
 export default function Header() {
     return (
         <div className="flex h-10">
-            <Image src={logo} alt="home" className="h-full w-auto"></Image>
+            <Link href="/">
+                <Image src={logo} alt="home" className="h-full w-auto"></Image>
+            </Link>
             <SearchBar/>
         </div>
     );
