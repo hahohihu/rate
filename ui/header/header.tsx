159b021375e82652715800ac5893d89a4c5a93e4
Peter Shen <pshen4@gmail.com>
2024-04-25 13:16:36 -0700

    add antd registry

--- ui/header/header.tsx
+++ ui/header/header.tsx
@@ -4,16 +4,17 @@ import SearchBar from "@/ui/searchbar/searchbar";
 import logo from '@/public/logo.svg';
 import Image from 'next/image'
 import Link from "next/link";
+import { Flex } from "antd";
 
 export default function Header() {
     return (
         <div className="h-12 bg-h1-c w-full p-2">
-            <div className="h-full flex max-w-5xl ml-auto mr-auto">
+            <Flex className="h-full max-w-5xl ml-auto mr-auto">
                 <Link href="/">
                     <Image src={logo} alt="home" className="h-full w-auto"></Image>
                 </Link>
-                <SearchBar className="w-full max-w-lg ml-auto" />
-            </div>
+                <SearchBar className="w-full max-w-lg !ml-auto" />
+            </Flex>
         </div>
     );
 }
