b487cb442c2fa1cb74c51e1e6f832760a3a0fdc2
Peter Shen <pshen4@gmail.com>
2024-04-25 22:22:46 -0700

    replace Flex with divs + flex

--- app/object/[id]/object-info.tsx
+++ app/object/[id]/object-info.tsx
@@ -1,15 +1,14 @@
 import { getObject } from "@/data/object";
-import { Flex } from "antd";
 
 export default async function ObjectInfo({ id, className }: { id: number, className?: string }) {
     let object = await getObject(id);
 
     return (
-        <Flex vertical className={className}>
+        <div className={`flex flex-col ${className}`}>
             <div className="flex items-end gap-2">
                 <span className="text-2xl font-normal text-fg-dominant leading-none">{object.name}</span>
                 <span className="text-fg-reach text-sm leading-none">{object.prod_year}</span>
             </div>
-        </Flex>
+        </div>
     );
 }
--- ui/header/header.tsx
+++ ui/header/header.tsx
@@ -9,12 +9,12 @@ import { Flex } from "antd";
 export default function Header() {
     return (
         <div className="h-12 bg-bg-top w-full">
-            <Flex className="h-full max-w-5xl ml-auto mr-auto p-2" gap="middle">
+            <div className="flex gap-2 h-full max-w-5xl ml-auto mr-auto p-2">
                 <Link href="/" className="min-h-full w-auto">
                     <Image src={logo} alt="home" className="min-h-full min-w-max"></Image>
                 </Link>
                 <SearchBar className="w-full max-w-lg !ml-auto" />
-            </Flex>
+            </div>
         </div>
     );
 }
