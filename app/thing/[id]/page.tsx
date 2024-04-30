ff8c5b420f96d6348544dcbb36c0547fbc6e03d6
Peter Shen <pshen4@gmail.com>
2024-04-30 14:06:08 -0700

    add rating spectrum and refactor modal

--- app/thing/[id]/page.tsx
+++ app/thing/[id]/page.tsx
@@ -1,14 +1,19 @@
 import ThingInfo from './thing-info';
 import Entries from './entries';
 import styles from './layout.module.css';
-import { EntryAddButton } from '@/ui/entry-editor/modal';
+import { ButtonModal } from '@/ui/entry-editor/modal';
+import { ThingHeader } from '@/ui/text';
+import { EntryAddForm } from '@/ui/entry-editor/form';
 
 export default async function Home({ params }: { params: { id: number } }) {
     return (
         <main className={`p-4 ${styles.thing_page}`}>
             <ThingInfo id={params.id} />
             <section className={styles.entries}>
-                <EntryAddButton ctx={{ thingId: params.id }} className={`${styles.add_entry} action-link`}>add a new entry</EntryAddButton>
+                <ButtonModal button='add a new entry' className={`${styles.add_entry} action-link`}>
+                    <ThingHeader thingId={params.id}></ThingHeader>
+                    <EntryAddForm ctx={{ thingId: params.id }}></EntryAddForm>
+                </ButtonModal>
                 <Entries thingId={params.id} className="p-4" />
             </section>
         </main>
