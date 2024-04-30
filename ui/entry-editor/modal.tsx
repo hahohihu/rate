e0d03a58d66a4f6f9e7b90bb21c41faea7c714a3
Peter Shen <pshen4@gmail.com>
2024-04-30 14:18:39 -0700

    re-add rating slider

--- ui/entry-editor/modal.tsx
+++ ui/entry-editor/modal.tsx
@@ -6,6 +6,7 @@ import { Entry, Thing } from "@/data/drizzle/schema";
 import { ThingTitle } from "../text";
 import { Form, Input, Modal } from "antd";
 import { addEntry } from "@/data/entry";
+import './editor.css';
 
 export function EntryAddButton({ ctx, className, children }: {
     ctx: {
@@ -31,17 +32,21 @@ export function EntryAddButton({ ctx, className, children }: {
         <>
             <button className={className} onClick={openModal}>{children}</button>
             <Modal open={modalOpen} onCancel={closeModal} footer={<div>Footer</div>}>
-                <div className={className}>
-                    <div className="flex gap-2 items-end mb-2">
-                        <ThingTitle className="text-2xl leading-none" name={thing?.name ?? "<name>"} />
-                        <span className="text-color-reach text-sm leading-none">{thing?.prod_year ?? "----"}</span>
-                    </div>
-                    <Form action={action}>
-                        <Form.Item>
-                            <Input></Input>
-                        </Form.Item>
-                    </Form>
+                <div className="flex gap-2 items-end mb-2">
+                    <ThingTitle className="text-2xl leading-none" name={thing?.name ?? "<name>"} />
+                    <span className="text-color-reach text-sm leading-none">{thing?.prod_year ?? "----"}</span>
                 </div>
+                <form action={action}>
+                    <div className="select-none">
+                        <label htmlFor="rating">rating</label>
+                        <input className="w-full" id="rating" type="range" min="-3" max="3" step="0.1" defaultValue="0" list="rating-values" />
+                        <datalist id="rating-values" className="flex w-full justify-between text-center">
+                            {
+                                [-3, -2, -1, 0, 1, 2, 3].map(key => (<option key={key} className="w-2 p-0" value={key}>{key}</option>))
+                            }
+                        </datalist>
+                    </div>
+                </form>
             </Modal>
         </>
     );
