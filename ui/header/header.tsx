a3dc9662e750edee9c1e033965cf600e4d7cf36b
Peter Shen <pshen4@gmail.com>
2024-05-02 14:59:41 -0700

    fix logo dimensions

--- ui/header/header.tsx
+++ ui/header/header.tsx
@@ -9,8 +9,8 @@ export default function Header() {
     return (
         <header className="bg-color-top w-full">
             <div className="flex gap-2 h-full max-w-5xl ml-auto mr-auto p-2">
-                <Link href="/" className="min-h-full w-auto" >
-                    <Image src={logo} alt="home" className="min-h-full min-w-max" />
+                <Link href="/" className="min-h-full min-w-max" >
+                    <Image src={logo} width="32" height="32" alt="home" className="min-h-full min-w-max" />
                 </Link>
                 <SearchBar className="w-full max-w-lg !ml-auto" />
             </div>
