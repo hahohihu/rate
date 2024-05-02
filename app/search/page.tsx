9a2d5ce14abac25a0cefdf2b7aecf5221871290d
Peter Shen <pshen4@gmail.com>
2024-05-02 13:21:28 -0700

    linting

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -4,28 +4,28 @@ import { PROVIDERS } from '@/data/provider/all';
 
 
 async function ProviderList(provider: Provider, query: string) {
-  let things = await provider.searchThings(query);
-  if (things.length == 0) {
-    return <></>
-  }
-  return <ProviderView matches={{ provider_name: provider.name, things }} />
+    let things = await provider.searchThings(query);
+    if (things.length == 0) {
+        return <></>
+    }
+    return <ProviderView matches={{ provider_name: provider.name, things }} />
 }
 
 export default async function Page({
-  searchParams,
+    searchParams,
 }: {
   searchParams?: {
     name?: string;
   }
 }) {
-  const query = searchParams?.name || '';
-  if (query == '') {
-    throw new Error("Empty search - display this a bit nicer");
-  }
+    const query = searchParams?.name || '';
+    if (query == '') {
+        throw new Error("Empty search - display this a bit nicer");
+    }
 
-  return (
-    <main className="p-4 space-y-4">
-      {PROVIDERS.map(provider => ProviderList(provider, query))}
-    </main>
-  );
+    return (
+        <main className="p-4 space-y-4">
+            {PROVIDERS.map(provider => ProviderList(provider, query))}
+        </main>
+    );
 }
