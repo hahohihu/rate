29eb23b7a211fe76d3cce68f382b1850cefb6c8c
Peter Shen <pshen4@gmail.com>
2024-05-02 14:52:39 -0700

    fix repeating background bug

--- ui/provider/provider.tsx
+++ ui/provider/provider.tsx
@@ -24,9 +24,10 @@ export function ProviderView({ matches }: { matches: ExternThings }) {
                     return <li key={i} className="border-b border-color-noise pb-2 max-w-[25rem]">
                         <div className="flex gap-2 items-center">
                             <button aria-label="Add new thing"
-                                className="w-[20px] h-[20px] bg-color-reach hover:bg-color-hover"
+                                className="min-w-[20px] min-h-[20px] bg-color-reach hover:bg-color-hover"
                                 style={{
                                     maskImage: `url(${AddIcon.src})`,
+                                    backgroundRepeat: "no-repeat"
                                 }}
                                 onClick={() => createNewThing()}
                                 ></button>
