7a03926c87f03e98e97a8015541942b88b449063
Peter Shen <pshen4@gmail.com>
2024-05-07 18:05:05 -0700

    autofix whitespace

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,51 +5,51 @@ import { useFormState, useFormStatus } from "react-dom";
 import AlertIcon from '@/public/alert-circle-error.svg';
 
 function LoginButton() {
-  const { pending } = useFormStatus();
+    const { pending } = useFormStatus();
 
-  return (
-    <button className="mt-4 p-2 rounded-md w-full bg-color-brand" aria-disabled={pending}>
+    return (
+        <button className="mt-4 p-2 rounded-md w-full bg-color-brand" aria-disabled={pending}>
       Log in
-    </button>
-  );
+        </button>
+    );
 }
 
 export default function LoginPage() {
-  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+    const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
-  return (
-    <main className="flex items-center justify-center h-full my-auto">
-      <form action={dispatch}>
-        <label
-          className="mb-1 block text-xs font-medium text-color-reach"
-          htmlFor="password"
-        >
+    return (
+        <main className="flex items-center justify-center h-full my-auto">
+            <form action={dispatch}>
+                <label
+                    className="mb-1 block text-xs font-medium text-color-reach"
+                    htmlFor="password"
+                >
           Password
-        </label>
-        <input
-          className="peer block w-full rounded-md border border-transparent py-[9px] pl-4 placeholder:text-color-noise bg-color-middle"
-          id="password"
-          type="password"
-          name="password"
-          placeholder="Enter password"
-          required
-        />
-        <LoginButton />
-        {
-          errorMessage && (
-            <div className="mt-4 p-2 rounded-md text-color-ordinary bg-red-600 flex gap-1 items-center">
-              <div
-                className="w-[20px] h-[20px] bg-color-ordinary text-color-ordinary border border-color-ordinary"
-                style={{
-                  maskImage: `url(${AlertIcon.src})`
-                }}></div>
-              <p>
-                {errorMessage}
-              </p>
-            </div>
-          )
-        }
-      </form>
-    </main >
-  );
+                </label>
+                <input
+                    className="peer block w-full rounded-md border border-transparent py-[9px] pl-4 placeholder:text-color-noise bg-color-middle"
+                    id="password"
+                    type="password"
+                    name="password"
+                    placeholder="Enter password"
+                    required
+                />
+                <LoginButton />
+                {
+                    errorMessage && (
+                        <div className="mt-4 p-2 rounded-md text-color-ordinary bg-red-600 flex gap-1 items-center">
+                            <div
+                                className="w-[20px] h-[20px] bg-color-ordinary text-color-ordinary border border-color-ordinary"
+                                style={{
+                                    maskImage: `url(${AlertIcon.src})`
+                                }}></div>
+                            <p>
+                                {errorMessage}
+                            </p>
+                        </div>
+                    )
+                }
+            </form>
+        </main >
+    );
 }
