6ff9c039b9496067bccc5cb16b26d3a2b09da131
Peter Shen <pshen4@gmail.com>
2024-05-09 22:19:08 -0700

    fix search mobile issue

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -15,7 +15,7 @@ export default async function Page({
     }
 
     return (
-        <main className="m-8 flex flex-col gap-6">
+        <main className="mt-8 md:m-8 flex flex-col gap-6">
             <NativeSearchView query={query} />
             {PROVIDERS.map(provider => <ProviderList key={provider.name} provider={provider} query={query} />)}
         </main>
